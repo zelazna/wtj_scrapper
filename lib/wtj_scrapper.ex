c09ed888f0114f47793205da6b54cfa87b895cd8
zelazna <constantin.guidon@gmail.com>
2019-05-09 12:04:44 +0200

    [feature] concurrent tasks

--- lib/wtj_scrapper.ex
+++ lib/wtj_scrapper.ex
@@ -2,12 +2,12 @@ defmodule WtjScrapper do
   @moduledoc """
   Documentation for WtjScrapper.
   """
+  alias HTTP.SplashClient
+  alias HTTP.WtjClient
+
   alias WtjScrapper.Parser
-  alias WtjScrapper.HTTP.SplashClient
-  alias WtjScrapper.HTTP.WtjClient
   alias WtjScrapper.Repo
   alias WtjScrapper.Models.Tag
-  alias WtjScrapper.Models.Job
 
   def run(tag_name) do
     SplashClient.start()
@@ -25,19 +25,7 @@ defmodule WtjScrapper do
       Application.get_env(:wtj_scrapper, :wtj_url) <> "/fr/jobs?query=#{tag_name}"
     ).body
     |> Parser.get_links()
-    |> Enum.map(&WtjClient.get!(&1))
-    |> Enum.map(&unserialize_job/1)
-    |> Enum.map(&save_job(%{&1 | tag_id: tag.id}))
-  end
-
-  defp unserialize_job(request) do
-    job_map = %{content: nil, tag_id: nil, title: nil, url: nil}
-    {_html, job} = Parser.parse_job(request.body, job_map)
-    %{job | url: request.request_url}
-  end
-
-  defp save_job(job) do
-    Job.changeset(%Job{}, job)
-    |> Repo.insert()
+    |> Enum.map(&Task.async(WtjScrapper.JobHandler, :get_job, [&1, tag]))
+    |> Enum.map(&Task.await(&1))
   end
 end
